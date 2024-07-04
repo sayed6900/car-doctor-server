c96ecf796acb1d01fe38808d462366a559723f74
Sayedul Islam <sayed.business03.com>
2024-07-04 23:13:10 +0600

    Checkout Page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000 ;
 
 //Middleware 
@@ -25,12 +25,40 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('Car-Doctor').collection('services')
+    const bookingCollection = client.db('Car-Doctor').collection('checkout')
+
+    // to find all the elements 
+    app.get('/services',async(req,res)=>{
+        const cursor = serviceCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+   
+    // to find just single one element
+    app.get('/services/:id', async(req,res)=>{
+        const id = req.params.id;
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: {  title: 1, price: 1 , service_id: 1},
+          };
+        const query = {_id: new ObjectId(id)};
+        const result = await serviceCollection.findOne(query)
+        res.send(result)
+    })
+
+     // Booking 
+     app.post('/checkout',async(req,res)=>{
+        const checkout = req.body
+     })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
